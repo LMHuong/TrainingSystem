cd4f28ebb489add2ea14d92d01acc0df9b2da753
Vu Manh Cuong <vmc529440@gmail.com>
2019-06-12 22:14:07 +0700

    tét

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?
 
 if (empty(getenv("DATABASE_URL"))){
-    $pdo = new PDO('pgsql:host=localhost;port=5432;dbname=mydb', 'postgres', '123456');
+    $pdo = new PDO('pgsql:host=localhost;port=3306;dbname=mydb', 'postgres', '123456');
 }  else {
   $db = parse_url(getenv("DATABASE_URL"));
   $pdo = new PDO("pgsql:" . sprintf(

4966f2f3ff9c32f2522bcf25890970c573dd9810
Lê Minh Hương <49479550+LMHuong@users.noreply.github.com>
2019-06-27 11:39:17 +0700

    yo

--- header-staff.php
+++ header-staff.php
@@ -75,7 +75,7 @@
                         echo "<a href='../Staff/view-trainer.php?username={$username}'><i class='fa fa-fw fa-table'></i> View Trainer</a>";
                       ?>
                     </li>
-                    /*<li>
+                    <li>
                       <?php
                         echo "<a href='../Staff/view-trainee.php?username={$username}'><i class='fa fa-fw fa-table'></i> View Trainee</a>";
                       ?>
@@ -84,7 +84,7 @@
                       <?php
                         echo "<a href='../Staff/add-trainee.php?username={$username}'><i class='fa fa-fw fa-table'></i> Add Trainee</a>";
                       ?>
-                    </li>*/
+                    </li>
                     <li>
                       <?php
                         echo "<a href='../Staff/add-topic.php?username={$username}'><i class='fa fa-fw fa-table'></i> Add Topic</a>";
